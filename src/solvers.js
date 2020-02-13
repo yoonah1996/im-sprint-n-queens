b960b34f0adca15800b39905d4155b3e2e034bf8
Jonghyeon Lee <leejonghyeon0106@gmail.com>
2020-02-13 23:23:33 +0900

    finished all

--- src/solvers.js
+++ src/solvers.js
@@ -182,11 +182,25 @@ window.countNRooksSolutions = function (n) {
   return solutionCount;
 };
 
-window.hasMajorDiagonalConflictAt= function(colI) {
+window._getFirstRowColumnIndexForMajorDiagonalOn = function(rowIndex, colIndex) {
+  return colIndex - rowIndex;
+},
+// 특정 좌표가 주어졌을 때, 해당 좌표를 지나는 반대각선의 첫 번째 행 컬럼을 반환합니다.
+// ex) rowIndex: 1, colIndex: 3이 주어졌을 때 4 반환
+//          -1 0 1 2 3 4
+// ----------------------
+//       0    [0,0,0,0]1
+//       1    [0,0,0,1]
+//       2    [0,0,1,0]
+//       3    [0,1,0,0]
+window._getFirstRowColumnIndexForMinorDiagonalOn = function(rowIndex, colIndex) {
+  return colIndex + rowIndex;
+},
+
+window.hasMajorDiagonalConflictAt= function(colI, n, arr) {
   let flag = false;
-  let arr = this.rows();
-  for(let row=0; row < arr.length; row++){
-    for(let col = colI; col < arr.length; col++){
+  for(let row=0; row < n; row++){
+    for(let col = colI; col < n; col++){
       if(colI === (col-row)){
         if(arr[row][col] === 1){
           if(flag === false){
@@ -202,12 +216,11 @@ window.hasMajorDiagonalConflictAt= function(colI) {
 }
 
 // 체스 판 위에 주대각선 충돌이 하나라도 있는지 검사합니다.
-window.hasAnyMajorDiagonalConflicts = function() {
-  let arr = this.rows();
-  for(let row=0; row<arr.length; row++){
-    for(let col=0; col<arr.length; col++){
+window.hasAnyMajorDiagonalConflicts = function(n, arr) {
+  for(let row=0; row<n; row++){
+    for(let col=0; col<n; col++){
       let majorDiagonalColumnIndexAtFirstRow = this._getFirstRowColumnIndexForMajorDiagonalOn(row,col);
-      if(this.hasMajorDiagonalConflictAt(majorDiagonalColumnIndexAtFirstRow) === true) return true;
+      if(this.hasMajorDiagonalConflictAt(majorDiagonalColumnIndexAtFirstRow,n,arr) === true) return true;
     }
   }
   return false; // fixme
@@ -217,10 +230,9 @@ window.hasAnyMajorDiagonalConflicts = function() {
 // --------------------------------------------------------------
 //
 // 주어진 반대각선에 충돌하는 말이 있는지 확인합니다.
-window.hasMinorDiagonalConflictAt = function(minorDiagonalColumnIndexAtFirstRow) {
+window.hasMinorDiagonalConflictAt = function(minorDiagonalColumnIndexAtFirstRow, n, arr) {
   let flag = false;
-  let arr = this.rows();
-  for(let row = 0; row < arr.length; row++){
+  for(let row = 0; row < n; row++){
     for(let col = minorDiagonalColumnIndexAtFirstRow; col >= 0; col--){
       if(minorDiagonalColumnIndexAtFirstRow === (row+col)){
         if(arr[row][col] === 1){
@@ -233,15 +245,15 @@ window.hasMinorDiagonalConflictAt = function(minorDiagonalColumnIndexAtFirstRow)
       }
     }
   }
+  return false; // fixme
 }
 
 // 체스 판 위에 반대각선 충돌이 하나라도 있는지 검사합니다.
-window.hasAnyMinorDiagonalConflicts = function() {
-  let arr = this.rows();
-  for(let row=0; row<arr.length; row++){
-    for(let col=0; col<arr.length; col++){
+window.hasAnyMinorDiagonalConflicts = function(n, arr) {
+  for(let row=0; row<n; row++){
+    for(let col=0; col<n; col++){
       let minorDiagonalColumnIndexAtFirstRow = this._getFirstRowColumnIndexForMinorDiagonalOn(row,col);
-      if(this.hasMinorDiagonalConflictAt(minorDiagonalColumnIndexAtFirstRow) === true) return true;
+      if(this.hasMinorDiagonalConflictAt(minorDiagonalColumnIndexAtFirstRow,n , arr) === true) return true;
     }
   }
   return false; // fixme
@@ -251,7 +263,7 @@ window.findNQueensMatrix = function (matrixTree, j , n) {
   for (let i = 0; i < n; i++) {   // 인자로 받은 matrixTree에 자식을 n개 넣기 위한 for문 작성
     let childSolution = JSON.parse(JSON.stringify(matrixTree.matrix)); // 깊은 복사를 위해 matrixTree의 matrix프로퍼티의 배열을 JSON.stringify를 통해 문자열로 변환 후 다시 JSON.parse를 통해 배열로 반환
     childSolution[j][i] = 1; // 복사된 부모 체스판에서 row가 j이고 col이 i인곳에 0대신 1을 넣는다(체스말 올리기)
-    if ((this.hasAnyRowConflicts(n, childSolution) === false) && (this.hasAnyColConflicts(n, childSolution) === false)) { // n rook이기 때문에 열과 행만 검사
+    if ((this.hasAnyRowConflicts(n, childSolution) === false) && (this.hasAnyColConflicts(n, childSolution) === false) && (this.hasAnyMinorDiagonalConflicts(n, childSolution) === false)&& (this.hasAnyMajorDiagonalConflicts(n, childSolution) === false)) { // n rook이기 때문에 열과 행만 검사
       if(j === n - 1) return childSolution; // 만약 j가 n-1이면 반환 (j는 열에 해당하는 index이고 n은 횟수이기 때문에 n-1은 인덱스의 마지막을 뜻함으로 j가 마지막 인덱스 즉 모든 열에 말을 위치했다고 가정)
       let childmatrix = new MatrixTree(childSolution); //부모에에 넣을 자식 생성
       if (j < n) { //열 index인 j가 n by n의 2차원 배열 안에 있는 값일 때 실행
@@ -270,6 +282,15 @@ window.findNQueensMatrix = function (matrixTree, j , n) {
 // 반환 값은 체스 판을 나타내는 2차원 배열입니다.
 window.findNQueensSolution = function (n) {
   var solution = [];  // 이차원 배열을 반환하기 위해 빈배열을 만듬
+  if(n === 0){
+    return solution;
+  }
+  
+  if(n === 2 || n === 3){
+    let board = new Board({n:n});
+    return board.rows();
+  }
+
   let newArr = [];  // solution배열에 넣을 일차 빈배열을 만듬
   for (let i = 0; i < n; i++) { // newarr에 0을 n번 넣기 위해서
     newArr.push(0);   // 0을 넣는문
@@ -280,9 +301,6 @@ window.findNQueensSolution = function (n) {
 
   let rootMatrix = new MatrixTree(solution); // 해답트리에 루트생성
   solution = this.findNQueensMatrix(rootMatrix, 0, n);
-  
-
-
   console.log(
     'Single solution for ' + n + ' queens:',
     JSON.stringify(solution),
